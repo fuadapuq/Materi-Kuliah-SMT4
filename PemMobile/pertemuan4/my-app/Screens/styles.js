27682ba370a17c747478ad885854f0e22d8d10a9
fuaddapuq <fuazis34@gmail.com>
2025-04-09 15:27:52 +0700

    pertemuan6

---
+++ PemMobile/pertemuan4/my-app/Screens/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    text: {
+        color: "black",
+        fontSize: 20,
+        fontFamily: "serif",
+    },
+    flex: {
+        display: "flex",
+        padding:10,
+    },
+});
